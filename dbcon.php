e77308eba7e8b278b8a0925854327e9f946c7adc
viancafrancisco <vitracerwang@gmail.com>
2025-04-23 21:15:35 -0900

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,6 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$con = mysqli_connect("localhost", "root", "", "trucking_services");
+$con = mysqli_connect("localhost", "root", "", "joredane_trucking_services");
 
 if (!$con) {
     die("Connection error: " . mysqli_connect_error());
